9a80299031b11f27296680fc870adc3436596600
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2021-07-31 19:33:42 -0500

    adding model index with table connections

--- models/index.js
+++ models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Category hasMany Product
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+})
+
 // Product belongsTo Category
-Product.
-    // Category hasMany Product
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+})
 
 // Product belongsToMany Tag (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag
+})
 
 // Tag belongsToMany Product (through ProductTag)
+Category.hasMany(Product, {
+    through: ProductTag
+})
 
 module.exports = {
     Product,
